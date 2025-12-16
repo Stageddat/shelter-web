3a13e3281492ff7ab9b889d1a7bedfcb2f856a4c
Stageddat <chmarc72@gmail.com>
2025-12-16 15:57:57 +0100

    fix: add padding in hero section

--- src/components/landing/sections/Hero.tsx
+++ src/components/landing/sections/Hero.tsx
@@ -4,7 +4,7 @@ import CtaButton from "@/components/landing/CtaButton";
 
 export default function Hero() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-x-16 border-2 border-black">
+    <main className="flex min-h-screen items-center justify-center gap-x-16 border-2 border-black px-16">
       <section className="text-left">
         <h1 className="m-0 text-9xl">shelter</h1>
         <p className="m-0 mt-2 text-2xl">
