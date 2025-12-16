424b4a9f8764a2ce76d1a202011126ba30ac6f1b
Stageddat <chmarc72@gmail.com>
2025-12-16 02:03:26 +0100

    feat: add hero

---
+++ src/components/landing/CtaButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../ui/button";
+
+export default function CtaButton() {
+  return (
+    <Button
+      variant="outline"
+      className="bg-primary rounded-xl px-12 py-8 text-xl font-bold"
+    >
+      start my journey!
+    </Button>
+  );
+}
---
+++ src/components/landing/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import heroImage from "@/assets/landing/hero-image.png";
+import CtaButton from "./CtaButton";
+
+export default function Hero() {
+  return (
+    <main className="flex min-h-screen items-center justify-center gap-x-16 border-2 border-black">
+      <section className="text-left">
+        <h1 className="m-0 text-9xl">shelter</h1>
+        <p className="m-0 mt-2 text-2xl">
+          a secure place where your thoughts can breathe
+        </p>
+        <div className="mt-4">
+          <CtaButton />
+        </div>
+      </section>
+
+      <Image src={heroImage} alt="shelter" width={500} height={500} />
+    </main>
+  );
+}
