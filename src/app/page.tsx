4c45fd7169846caa4cc02280d61f5fb159fae37c
Stageddat <chmarc72@gmail.com>
2025-12-16 21:01:25 +0100

    feat: add Features Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Hero from "@/components/landing/sections/Hero";
-
+import Features from "@/components/landing/sections/Features";
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  );
 }
