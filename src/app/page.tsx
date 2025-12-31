b3b7f01f66e14223ad2415586e3d8b3c35b2e678
Stageddat <chmarc72@gmail.com>
2025-12-31 14:11:39 +0100

    feat: set bg landing page as gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import Footer from "@/components/landing/sections/Footer";
 
 export default function Home() {
   return (
-    <>
+    <div className="min-h-screen bg-[linear-gradient(165deg,#f5ebe0_0%,#e8f4f8_30%,#ffffff_70%,#f8f8f8_100%)]">
       <Hero />
       <Features />
       <Security />
       <Start />
       <Footer />
-    </>
+    </div>
   );
 }
