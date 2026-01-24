8e17ab4785d885251a2cf5c6cb2f023d82ccc788
Stageddat <chmarc72@gmail.com>
2026-01-24 23:54:27 +0100

    feat: add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,97 +7,26 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Mail, Lock } from "lucide-react";
+import { getUser } from "@/services/auth/getUser";
+import LoginForm from "@/components/login/loginForm";
 
 export default function Login() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+  // TODO: login
+  // cargar los usuarios si hay y mostrar solo form de contraseña
+  // añadir aviso si no se detecta usuario
+  // redirigir a dashboard al logearse
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  // TODO: separar en diferentes views el FORM
+  // 1. login mail y pass
+  // 2. login user y pass
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError("");
-    setIsLoading(true);
-
-    // TODO: login
-    // cargar los usuarios si hay y mostrar solo form de contraseña
-    // añadir aviso si no se detecta usuario
-    // redirigir a dashboard al logearse
-
-    // TODO: separar en diferentes views el FORM
-    // 1. login mail y pass
-    // 2. login user y pass
-  };
   return (
     <div>
-      <h1>login</h1>
+      <h1>LOGIN!!!!</h1>
       <div>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="space-y-2">
-            {/* ask mail */}
-            <Label htmlFor="email">email</Label>
-            <InputGroup>
-              <InputGroupInput
-                type="email"
-                placeholder="enter your email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="h-12 text-base"
-                required
-                disabled={isLoading}
-              />
-              <InputGroupAddon>
-                <Mail />
-              </InputGroupAddon>
-            </InputGroup>
-
-            {/* ask password */}
-            <Label htmlFor="password">password</Label>
-            <InputGroup>
-              <InputGroupInput
-                type="password"
-                placeholder="set a password"
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                className="h-12 text-base"
-                required
-                disabled={isLoading}
-              />
-              <InputGroupAddon>
-                <Lock />
-              </InputGroupAddon>
-            </InputGroup>
-
-            {/* error message */}
-            {error && (
-              <p className="text-destructive text-sm font-medium">{error}</p>
-            )}
-
-            <Button
-              type="submit"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 mt-4 w-full"
-              size="lg"
-              disabled={isLoading}
-            >
-              {isLoading ? "logging in..." : "continue"}
-            </Button>
-          </div>
-        </form>
+        <LoginForm />
       </div>{" "}
     </div>
   );
---
+++ src/components/login/loginForm.tsx
@@ -0,0 +1,38 @@
+import { useLoginForm } from "@/hooks/login/useLoginForm";
+import UserLogin from "./userLogin";
+import DefaultLogin from "./defaultLogin";
+
+export default function LoginForm() {
+  const {
+    userData,
+    loginFormData,
+    handleSubmit,
+    handleChange,
+    isLoading,
+    error,
+  } = useLoginForm();
+
+  return (
+    <>
+      {/* renderizar login dependiendo de si hay o no usuario */}
+      {userData !== null ? (
+        <UserLogin
+          loginFormData={loginFormData}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          isLoading={isLoading}
+          error={error}
+          username={userData.username}
+        />
+      ) : (
+        <DefaultLogin
+          loginFormData={loginFormData}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          isLoading={isLoading}
+          error={error}
+        />
+      )}
+    </>
+  );
+}
