ce84c5044fb89419a92ace2555e78a7a63dcf626
Stageddat <chmarc72@gmail.com>
2026-01-25 02:01:46 +0100

    feat: make login return master key

--- src/services/auth/login.ts
+++ src/services/auth/login.ts
@@ -1,35 +1,54 @@
-import { checkPassword } from "./checkPassword";
+// @/services/auth/login.ts
 import { getUser } from "@/services/auth/getUser";
+import { decryptMasterKey, deriveKeyFromPassword } from "@/lib/crypto";
 
 export interface LoginCredentials {
   email?: string;
   password: string;
 }
 
-export async function login(credentials: LoginCredentials): Promise<boolean> {
+export interface LoginResult {
+  success: boolean;
+  masterKey?: CryptoKey;
+}
+
+export async function login(
+  credentials: LoginCredentials,
+): Promise<LoginResult> {
   try {
     const user = await getUser();
 
     if (!user) {
-      throw new Error("No user found");
+      throw new Error("no user found");
     }
 
-    // si se proporciona email, verificar que coincida
+    // comprovar correo
     if (credentials.email && user.email !== credentials.email) {
       throw new Error("email does not match");
     }
 
-    // verificar la contraseña
-    const isPasswordValid = await checkPassword(
+    // derivar clave
+    const passwordKey = await deriveKeyFromPassword(
       credentials.password,
       user.salt,
-      user.encryptedMasterKey,
-      user.iv,
     );
 
-    return isPasswordValid;
+    // intentar desencriptar la masterKey
+    const masterKey = await decryptMasterKey(
+      user.encryptedMasterKey,
+      user.iv,
+      passwordKey,
+    );
+
+    // todo bien aqui??? la contraseña es correcta
+    return {
+      success: true,
+      masterKey,
+    };
   } catch (error) {
     console.error("login error:", error);
-    return false;
+    return {
+      success: false,
+    };
   }
 }
