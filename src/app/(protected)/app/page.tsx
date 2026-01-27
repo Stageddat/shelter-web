95c27814a94a76b9e98b06641870d6db8a13d766
Stageddat <chmarc72@gmail.com>
2026-01-27 13:46:56 +0100

    fix: fix app flicking when user is not logged in

--- src/app/(protected)/app/page.tsx
+++ src/app/(protected)/app/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
 import { useAuth } from "@/contexts/AuthContext";
 import { useRouter } from "next/navigation";
 
 export default function App() {
   const { masterKey, isAuthenticated } = useAuth();
   const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login");
+    }
+  }, [isAuthenticated, router]);
+
   if (!isAuthenticated) {
-    router.push("/login");
+    return null;
   }
 
   return (
