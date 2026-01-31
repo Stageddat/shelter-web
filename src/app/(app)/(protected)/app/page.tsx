5937da1b3c9b0418f6ab5cd40bda2c27cba2613d
Stageddat <chmarc72@gmail.com>
2026-01-31 23:35:21 +0100

    feat: remove login logic

--- src/app/(app)/(protected)/app/page.tsx
+++ src/app/(app)/(protected)/app/page.tsx
@@ -1,28 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
-import { useAuth } from "@/contexts/AuthContext";
-import { useRouter } from "next/navigation";
-
 import { Sidebar } from "@/components/app/Sidebar";
-import { Header } from "@/components/Header";
+import { Header } from "@/components/app/Header";
 // import { JournalEntry } from "@/components/JournalEntry";
 // import { EntryActions } from "@/components/EntryActions";
 
 export default function App() {
-  // const { masterKey, isAuthenticated } = useAuth();
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     router.push("/login");
-  //   }
-  // }, [isAuthenticated, router]);
-
-  // if (!isAuthenticated) {
-  //   return null;
-  // }
-
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
