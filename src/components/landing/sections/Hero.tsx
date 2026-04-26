783d4445ac9f479b4f93f801b94ce22ad6174689
stageddat <chmarc72@gmail.com>
2026-04-26 02:48:17 +0200

    style: adjust dimensions of background elements in Hero section for improved layout

--- src/components/landing/sections/Hero.tsx
+++ src/components/landing/sections/Hero.tsx
@@ -4,7 +4,7 @@ import heroImage from "@/assets/landing/machka.png";
 export default function Hero() {
   return (
     <section className="bg-background relative flex min-h-screen w-full items-center justify-center overflow-hidden px-8">
-      <div className="bg-secondary/40 absolute top-[-5%] left-[-5%] -z-10 h-[600px] w-[600px] rounded-full blur-[130px]" />
+      <div className="bg-secondary/40 absolute top-[-5%] left-[-5%] -z-10 h-150 w-150 rounded-full blur-[130px]" />
       <div className="bg-accent/20 absolute right-[5%] bottom-[5%] -z-10 h-125 w-125 rounded-full blur-[110px]" />
 
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center gap-0 pt-32 pb-20 lg:flex-row lg:items-center lg:pt-0 lg:pb-0">
@@ -22,7 +22,7 @@ export default function Hero() {
 
         {/* img */}
         <div className="flex flex-1 items-center justify-center lg:-ml-16 lg:justify-center">
-          <div className="relative w-full max-w-[500px] sm:max-w-[600px] lg:max-w-[700px]">
+          <div className="relative w-full max-w-125 sm:max-w-150 lg:max-w-175">
             <Image
               src={heroImage}
               alt="shelter mascot"
