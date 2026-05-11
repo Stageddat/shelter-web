83f03e2626d31a0c0ee612e83a7064817344f76d
Stageddat <hello@stageddat.dev>
2026-05-12 00:21:50 +0200

    feat: update formFields to use RegisterInput type and remove unused fields

--- src/data/signup/formFields.ts
+++ src/data/signup/formFields.ts
@@ -1,8 +1,9 @@
-import { User, Mail, Cake, Lock, CircleQuestionMark, Info } from "lucide-react";
+import { RegisterInput } from "@/schemas/register.schema";
+import { User, Lock, Info } from "lucide-react";
 import { LucideIcon } from "lucide-react";
 
 export interface FormFieldConfig {
-  id: string;
+  id: keyof RegisterInput;
   name: string;
   label: string;
   type: string;
@@ -23,26 +24,6 @@ export const formFields: FormFieldConfig[] = [
     tooltipIcon: Info,
     tooltipContent:
       "this is how we will call you across the app, you don't have to use your real name",
-  },
-  {
-    id: "email",
-    name: "email",
-    label: "email",
-    type: "email",
-    placeholder: "enter your email",
-    icon: Mail,
-    tooltipIcon: CircleQuestionMark,
-    tooltipContent: "we only use your email to send you important updates.",
-  },
-  {
-    id: "dateOfBirth",
-    name: "dateOfBirth",
-    label: "date of birth",
-    type: "date",
-    icon: Cake,
-    tooltipIcon: CircleQuestionMark,
-    tooltipContent:
-      "we only use this date to estimate your age for legal purposes.",
   },
   {
     id: "password",
