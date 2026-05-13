1f6a08f4dfc4c42b8f72e32e884e271a6d1d7313
Stageddat <hello@stageddat.dev>
2026-05-13 09:55:28 +0200

    feat: update SignupHeader to increase bottom margin for improved spacing

--- src/components/signup/SignupHeader.tsx
+++ src/components/signup/SignupHeader.tsx
@@ -1,7 +1,7 @@
 export default function SignupHeader() {
   return (
     <div className="space-y-2 text-center">
-      <h1 className="text-foreground mb-4 text-4xl font-bold tracking-tight lg:text-5xl">
+      <h1 className="text-foreground mb-8 text-4xl font-bold tracking-tight lg:text-5xl">
         welcome to shelter!
       </h1>
     </div>
