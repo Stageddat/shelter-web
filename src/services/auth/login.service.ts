c337ff2e68abddf82c561f3ce2fa8dac97ed8647
Stageddat <hello@stageddat.dev>
2026-05-25 02:24:38 +0200

    refactor: simplify login function by removing unnecessary interfaces and improving error handling

--- src/services/auth/login.service.ts
+++ src/services/auth/login.service.ts
@@ -1,54 +1,22 @@
-// @/services/auth/login.ts
-import { getUser } from "@/services/auth/getUser";
+import { getUser } from "@/lib/db.utils";
 import { decryptMasterKey, deriveKeyFromPassword } from "@/lib/crypto";
 
-export interface LoginCredentials {
-  email?: string;
-  password: string;
-}
+export async function login(password: string): Promise<CryptoKey> {
+  const user = await getUser();
 
-export interface LoginResult {
-  success: boolean;
-  masterKey?: CryptoKey;
-}
+  if (!user) {
+    throw new Error("no account found");
+  }
 
-export async function login(
-  credentials: LoginCredentials,
-): Promise<LoginResult> {
+  const passwordKey = await deriveKeyFromPassword(password, user.salt);
+
   try {
-    const user = await getUser();
-
-    if (!user) {
-      throw new Error("no user found");
-    }
-
-    // comprovar correo
-    // if (credentials.email && user.email !== credentials.email) {
-    //   throw new Error("email does not match");
-    // }
-
-    // derivar clave
-    const passwordKey = await deriveKeyFromPassword(
-      credentials.password,
-      user.salt,
-    );
-
-    // intentar desencriptar la masterKey
-    const masterKey = await decryptMasterKey(
+    return await decryptMasterKey(
       user.encryptedMasterKey,
       user.iv,
       passwordKey,
     );
-
-    // todo bien aqui??? la contraseña es correcta
-    return {
-      success: true,
-      masterKey,
-    };
-  } catch (error) {
-    console.error("login error:", error);
-    return {
-      success: false,
-    };
+  } catch {
+    throw new Error("incorrect password");
   }
 }
