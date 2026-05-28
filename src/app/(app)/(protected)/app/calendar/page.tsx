bf783d92530baa07d7af7a8f545d2426973063dd
Stageddat <hello@stageddat.dev>
2026-05-28 03:18:10 +0200

    feat: add new pages for Calendar, Search, Settings, and Stats

---
+++ src/app/(app)/(protected)/app/calendar/page.tsx
@@ -0,0 +1,7 @@
+export default function Calendar() {
+  return (
+    <p className="flex min-h-screen items-center justify-center text-6xl">
+      coming soon :3
+    </p>
+  );
+}
---
+++ src/app/(app)/(protected)/app/stats/page.tsx
@@ -0,0 +1,7 @@
+export default function Search() {
+  return (
+    <p className="flex min-h-screen items-center justify-center text-6xl">
+      ₍^. .^₎⟆
+    </p>
+  );
+}
