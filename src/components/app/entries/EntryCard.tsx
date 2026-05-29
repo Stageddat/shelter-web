76dee5b25011429196b18533c0e6b1bc7688af89
Stageddat <hello@stageddat.dev>
2026-05-29 23:07:53 +0200

    feat: implement delete feature in entries

--- src/components/app/entries/EntryCard.tsx
+++ src/components/app/entries/EntryCard.tsx
@@ -1,16 +1,46 @@
 import Link from "next/link";
-import { DecryptedEntry } from "@/services/app/entry.service";
+import { DecryptedEntry, deleteEntry } from "@/services/app/entry.service";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import { useEntries } from "@/contexts/EntriesContext";
 
 export default function EntryCard({ entry }: { entry: DecryptedEntry }) {
+  const { refreshEntries } = useEntries();
+
+  function handleDelete(id: string) {
+    if (
+      confirm(
+        `are you sure you want to delete \"${entry.title}\"?\nthis entry will be lost forever! (a long time!)`,
+      )
+    ) {
+      deleteEntry(id);
+      refreshEntries();
+    }
+  }
+
   return (
     <Link
       href={`/app/entries/${entry.id}`}
-      className="hover:bg-accent border-border group block rounded-xl border p-4 transition-all"
+      className="hover:bg-accent border-border group flex items-center rounded-xl border p-4 transition-all"
     >
-      <p className="text-xl">{entry.title}</p>
-      <p className="text-muted-foreground mt-1 text-base">
-        {entry.date} · {entry.time}
-      </p>
+      <div className="flex-1">
+        <p className="text-xl">{entry.title}</p>
+        <p className="text-muted-foreground mt-1 text-base">
+          {entry.date} · {entry.time}
+        </p>
+      </div>
+
+      <Button
+        variant="ghost"
+        className="hover:text-destructive hover:bg-destructive/20 mr-2 hidden h-12 w-12 cursor-pointer items-center justify-center rounded-full p-0! group-hover:flex"
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          handleDelete(entry.id);
+        }}
+      >
+        <Trash className="h-6! w-6! stroke-2" />
+      </Button>
     </Link>
   );
 }
