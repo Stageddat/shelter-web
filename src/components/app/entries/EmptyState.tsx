f90d3373a5d2f9aa78aa39f46a8339f8bcae9f2c
Stageddat <hello@stageddat.dev>
2026-05-30 23:23:50 +0200

    style: redign entries page and delete entry modal

--- src/components/app/entries/EmptyState.tsx
+++ src/components/app/entries/EmptyState.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function EmptyState() {
   return (
-    <div className="text-muted-foreground flex h-screen flex-col items-center justify-center">
+    <div className="text-muted-foreground -mt-52 flex h-screen flex-col items-center justify-center">
       <p className="text-4xl lowercase">no entries yet</p>
-      <p className="mt-2 text-3xl">^▽^</p>
+      <p className="mt-2 mb-4 text-3xl">^▽^</p>
+      <Button asChild className="p-5! text-xl! tracking-wide">
+        <Link href="/app/new">begin the story</Link>
+      </Button>
     </div>
   );
 }
