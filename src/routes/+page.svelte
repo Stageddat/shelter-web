2313563f7514d9116a9745d5977238c3beda5c29
Stageddat <hello@stageddat.dev>
2026-06-02 01:42:25 +0200

    feat: migrate navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,12 @@
 	import Features from '$lib/components/landing/sections/Features.svelte';
 	import Footer from '$lib/components/landing/sections/Footer.svelte';
 	import Hero from '$lib/components/landing/sections/Hero.svelte';
+	import Navbar from '$lib/components/landing/sections/Navbar.svelte';
 	import Security from '$lib/components/landing/sections/Security.svelte';
 	import Start from '$lib/components/landing/sections/Start.svelte';
 </script>
 
+<Navbar />
 <Hero />
 <Features />
 <Security />
