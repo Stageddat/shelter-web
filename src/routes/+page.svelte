69351b7ce001485b53bc019b3582b588889e5cac
Stageddat <hello@stageddat.dev>
2026-06-01 19:48:52 +0200

    feat: implement security section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Features from '$lib/components/landing/sections/Features.svelte';
 	import Hero from '$lib/components/landing/sections/Hero.svelte';
+	import Security from '$lib/components/landing/sections/Security.svelte';
 </script>
 
 <Hero />
 <Features />
+<Security />
