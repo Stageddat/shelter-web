dc894833c5d3f9b8530a2d99a8f51e50b42d26b1
Stageddat <hello@stageddat.dev>
2026-06-01 19:13:44 +0200

    feat: migrate hero

---
+++ src/lib/components/landing/sections/Hero.svelte
@@ -0,0 +1,45 @@
+<script>
+	import MachkaImg from '$lib/assets/landing/machka.png';
+</script>
+
+<section
+	class="relative flex min-h-screen w-full items-center justify-center overflow-hidden bg-background px-8"
+>
+	<div
+		class="absolute top-[-5%] left-[-5%] -z-10 h-150 w-150 rounded-full bg-secondary/40 blur-[130px]"
+	/>
+	<div
+		class="absolute right-[5%] bottom-[5%] -z-10 h-125 w-125 rounded-full bg-accent/20 blur-[110px]"
+	/>
+
+	<div
+		class="mx-auto flex w-full max-w-7xl flex-col items-center justify-center gap-0 pt-32 pb-20 lg:flex-row lg:items-center lg:pt-0 lg:pb-0"
+	>
+		<!-- {/* text left */} -->
+		<div class="flex flex-1 flex-col items-center text-center lg:items-start lg:text-left">
+			<div class="space-y-8">
+				<h1
+					class="font-display text-8xl leading-[0.8] font-normal tracking-wide text-foreground sm:text-9xl lg:text-[13rem]"
+				>
+					shelter
+				</h1>
+				<p
+					class="font-primary mx-auto max-w-md text-left text-xl leading-relaxed text-pretty text-muted-foreground lg:mx-0 lg:text-3xl"
+				>
+					a secure place where your thoughts can breathe
+				</p>
+			</div>
+		</div>
+
+		<!-- {/* img */} -->
+		<div class="flex flex-1 items-center justify-center lg:-ml-16">
+			<div class="relative w-full max-w-125 sm:max-w-150 lg:max-w-175">
+				<enhanced:img
+					src={MachkaImg}
+					alt="machka writing her journal with her kitty"
+					class="h-auto w-full object-contain brightness-[1.02] transition-all duration-1000"
+				/>
+			</div>
+		</div>
+	</div>
+</section>
