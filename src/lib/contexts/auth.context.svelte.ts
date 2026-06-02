4dd090f7d0b755cbd39478f1297eef3a6c85db46
Stageddat <hello@stageddat.dev>
2026-06-02 16:18:48 +0200

    feat: migrate auth context

---
+++ src/lib/contexts/auth.context.svelte.ts
@@ -0,0 +1,19 @@
+import { createContext } from 'svelte';
+import { type User } from '$lib/db';
+import { getUser } from '$lib/db.utils';
+
+class AuthState {
+	user = $state<User | undefined>(undefined);
+	masterKey = $state<CryptoKey | null>(null);
+	isAuthenticated = $derived(this.masterKey !== null);
+
+	constructor() {
+		getUser().then((u) => (this.user = u));
+	}
+
+	logout = () => {
+		this.masterKey = null;
+	};
+}
+
+export const [getAuthContext, setAutContexth] = createContext<AuthState>();
---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { getAuthContext, setAutContexth } from '$lib/contexts/auth.context.svelte';
+	let { children } = $props();
+
+	getAuthContext();
+</script>
+
+{@render children()}
