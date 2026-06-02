bc6f3722a4b75020eca041844e203d012cffa7b1
Stageddat <hello@stageddat.dev>
2026-06-02 02:49:21 +0200

    feat: migrate back to home link

---
+++ src/lib/components/landing/BackToHome.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Button from '../ui/button/button.svelte';
+	import ArrowLeft from '@lucide/svelte/icons/arrow-left';
+</script>
+
+<a href="/" class="text-primary-dark flex flex-row items-center underline-offset-4 hover:underline">
+	<ArrowLeft class="mr-2 h-4 w-4" />
+	go back to home
+</a>
