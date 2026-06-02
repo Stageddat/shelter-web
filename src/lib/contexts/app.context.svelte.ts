9185bac09ec68f19447a65061c1183298e068e11
Stageddat <hello@stageddat.dev>
2026-06-02 18:06:59 +0200

    feat: implement app context

---
+++ src/lib/contexts/app.context.svelte.ts
@@ -0,0 +1,49 @@
+import { createContext } from 'svelte';
+import { getEntries, type DecryptedEntry } from '$lib/services/app/entry.service';
+import { getAuthContext } from '$lib/contexts/auth.context.svelte';
+// import { getDynamicGreeting } from '$hooks/app/useDynamicGreetings';
+
+class EntriesState {
+	entries = $state<DecryptedEntry[]>([]);
+	isLoading = $state(true);
+
+	greeting = $derived.by(() => {
+		const auth = getAuthContext();
+		// return getDynamicGreeting(auth?.user?.username);
+	});
+
+	constructor() {
+		const auth = getAuthContext();
+
+		$effect(() => {
+			if (!auth || !auth.masterKey) return;
+
+			this.loadEntries(auth.masterKey);
+		});
+	}
+
+	private async loadEntries(masterKey: CryptoKey) {
+		try {
+			this.isLoading = true;
+			const data = await getEntries(masterKey);
+			this.entries = data;
+		} catch (error) {
+			console.error('error loading entries:', error);
+		} finally {
+			this.isLoading = false;
+		}
+	}
+
+	refreshEntries = async () => {
+		const auth = getAuthContext();
+		if (!auth || !auth.masterKey) return;
+		await this.loadEntries(auth.masterKey);
+	};
+
+	removeEntry = (entryId: string) => {
+		this.entries = this.entries.filter((e) => e.id !== entryId);
+	};
+}
+
+export const [getEntriesContext, setEntriesContext] = createContext<EntriesState>();
+export { EntriesState };
