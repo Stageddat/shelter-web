c54227e72e147baf7d2fd2943bd1da478377cb8a
Stageddat <hello@stageddat.dev>
2026-06-03 23:32:47 +0200

    feat: add Catalan localization and update HTML language attributes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js'
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' }),enhancedImages(), sveltekit(), tailwindcss()]
+	plugins: [
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/paraglide',
+			strategy: ['url', 'cookie', 'baseLocale']
+		}),
+		enhancedImages(),
+		sveltekit(),
+		tailwindcss()
+	]
 	// build: { sourcemap: true }
 });
