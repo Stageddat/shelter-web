2b6f0355fc949060d1f95f740f7c84f03db34190
Stageddat <hello@stageddat.dev>
2026-06-03 23:22:28 +0200

    feat: add inland config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit(), tailwindcss()]
+	plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' }),enhancedImages(), sveltekit(), tailwindcss()]
 	// build: { sourcemap: true }
 });
