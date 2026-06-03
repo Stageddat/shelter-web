7179804b36c539251c89ba7533370d86236a424f
Stageddat <hello@stageddat.dev>
2026-06-03 19:19:52 +0200

    feat: migrate empty state

---
+++ src/lib/components/app/entry/EmptyState.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="-mt-52 flex h-screen flex-col items-center justify-center text-muted-foreground">
+	<p class="text-4xl lowercase">no entries yet</p>
+	<p class="mt-2 mb-4 text-3xl">^▽^</p>
+	<Button class="p-5! text-xl! tracking-wide" href="/app/new">begin the story</Button>
+</div>
