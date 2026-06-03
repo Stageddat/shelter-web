c8b38c85c519f61c2ed6ad0d50026b9f04f5a64c
Stageddat <hello@stageddat.dev>
2026-06-03 19:26:42 +0200

    feat: update link resolution in BackToHome component

--- src/lib/components/landing/BackToHome.svelte
+++ src/lib/components/landing/BackToHome.svelte
@@ -1,9 +1,12 @@
 <script>
-	import Button from '../ui/button/button.svelte';
+	import { resolve } from '$app/paths';
 	import ArrowLeft from '@lucide/svelte/icons/arrow-left';
 </script>
 
-<a href="/" class="text-primary-dark flex flex-row items-center underline-offset-4 hover:underline">
+<a
+	href={resolve('/')}
+	class="text-primary-dark flex flex-row items-center underline-offset-4 hover:underline"
+>
 	<ArrowLeft class="mr-2 h-4 w-4" />
 	go back to home
 </a>
