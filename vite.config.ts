49988d200b4ab69deb21d042f1c5de37ce398f42
Stageddat <hello@stageddat.dev>
2026-06-04 02:00:50 +0200

    feat: reorder localization strategy for paraglide plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
-			strategy: ['localStorage', 'url', 'preferredLanguage', 'baseLocale'],
+			strategy: ['url', 'localStorage', 'preferredLanguage', 'baseLocale'],
 			routeStrategies: [
 				{
 					match: '/app/:path(.*)?',
