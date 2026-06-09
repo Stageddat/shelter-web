9f6b779395cdd7d2a7697b6d92b66e117f2460df
Stageddat <hello@stageddat.dev>
2026-06-10 00:00:52 +0200

    fix: update background color of entry view to match dark mode theme

--- src/lib/components/app/entry/EntryView.svelte
+++ src/lib/components/app/entry/EntryView.svelte
@@ -99,7 +99,7 @@
 	/>
 </div>
 
-<div class="flex min-h-0 flex-1 flex-col rounded-sm border-2 bg-white/80 py-2">
+<div class="flex min-h-0 flex-1 flex-col rounded-sm border-2 bg-secondary/10 py-2">
 	{#if displayedEntry}
 		<Editor
 			bind:this={editorRef}
