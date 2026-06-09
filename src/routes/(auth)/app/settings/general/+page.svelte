cb65b79e2a7b77fae92a58db2f43fd64137d0077
Stageddat <hello@stageddat.dev>
2026-06-09 23:59:43 +0200

    fix: correct appearance section layout and improve theme/language labels in settings

--- src/routes/(auth)/app/settings/general/+page.svelte
+++ src/routes/(auth)/app/settings/general/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import LanguageSelector from '$lib/components/landing/LanguageSelector.svelte';
 	import ThemeSelector from '$lib/components/shared/ThemeSelector.svelte';
 </script>
 
@@ -13,12 +14,18 @@
 
 	<!-- project info -->
 	<div class="mb-6">
-		<h2 class="mb-1 flex text-left text-3xl font-semibold tracking-wider lowercase">appereance</h2>
-		<div class="flex flex-row justify-between gap-2">
-			<p class="mb-0 text-left text-xl tracking-wide lowercase opacity-70">theme</p>
+		<h2 class="mb-2 flex text-left text-3xl font-semibold tracking-wider lowercase">appereance</h2>
+		<div class="flex flex-row items-center justify-between gap-2">
+			<p class="text-xl tracking-wide lowercase opacity-85">theme</p>
 			<ThemeSelector />
 		</div>
+		<hr class="my-4 border-current opacity-10" />
+		<div class="flex flex-row items-center justify-between gap-2">
+			<div>
+				<p class="text-xl tracking-wide lowercase opacity-85">language</p>
+				<p class="text-base tracking-wide lowercase opacity-60">this will reload the page!</p>
+			</div>
+			<LanguageSelector />
+		</div>
 	</div>
-
-	<hr class="border-current opacity-10" />
 </div>
