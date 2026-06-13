8b0b6ab81c43736a21c8731dc06b9972852ccaf3
Stageddat <hello@stageddat.dev>
2026-06-13 23:27:46 +0200

    feat: restrict access to test page for non-development environments

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
 		if (!import.meta.env.DEV) {
-			goto('/');
+			throw new Error('not allowed');
 		}
 	});
 </script>
